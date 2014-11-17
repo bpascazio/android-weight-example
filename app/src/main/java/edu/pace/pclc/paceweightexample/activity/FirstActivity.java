82d1d8ca0009a65381ce81c2f3e44efbde9e184c
SSCSIS <sscsis@DT-336-I.local>
2014-11-17 18:05:08 -0500

    first commit

---
+++ app/src/main/java/edu/pace/pclc/paceweightexample/activity/FirstActivity.java
@@ -0,0 +1,61 @@
+package edu.pace.pclc.paceweightexample.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import edu.pace.pclc.paceweightexample.R;
+
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void secondActivityClick(View v)
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void thirdActivityClick(View v)
+    {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
+
+    public void fourthActivityClick(View v)
+    {
+        Intent intent = new Intent(this, FourthActivity.class);
+        startActivity(intent);
+    }
+}
